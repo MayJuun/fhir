a35efc795e96dafd68d9475ba9516dc58b0760b5
Dokotela <jgfaulkenberry@gmail.com>
2020-05-12 15:52:05 -0400

    Trying to complete serialization

--- lib/fhir_r4.dart
+++ lib/fhir_r4.dart
@@ -1,6 +1,2 @@
-export 'r4/draft_types/draft_types.dart';
-export 'r4/general_types/general_types.dart';
-export 'r4/metadata_types/metadata_types.dart';
+export 'r4/resources.dart';
 export 'primitiveTypes/primitiveTypes.dart';
-// export 'r4/resource_types/types.dart';
-export 'r4/special_types/special_types.dart';
--- lib/r4/resource_types/resource_types.dart
+++ lib/r4/resource_types/resource_types.dart
@@ -6,7 +6,6 @@ part 'resource_types.freezed.dart';
 @freezed
 abstract class ResourceTypes with _$ResourceTypes {
   const factory ResourceTypes() = _ResourceTypes;
-  //todo: care plans
 
   factory ResourceTypes.fromJson(Map<String, dynamic> json) =>
       _$ResourceTypesFromJson(json);
