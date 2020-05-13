443013516ebaa440943d235192985c68d391e2c6
Dokotela <jgfaulkenberry@gmail.com>
2020-05-12 22:42:30 -0400

    Always forget to change bundleentry resource

--- lib/r4/resource_types/foundation/other/other.g.dart
+++ lib/r4/resource_types/foundation/other/other.g.dart
@@ -862,7 +862,8 @@ _$BundleEntry _$_$BundleEntryFromJson(Map<String, dynamic> json) {
     fullUrl: json['fullUrl'] == null
         ? null
         : FhirUri.fromJson(json['fullUrl'] as String),
-    resource: json['resource'],
+    resource:
+        json['resource'] == null ? null : ResourceFromJson(json['resource']),
     search: json['search'] == null
         ? null
         : BundleSearch.fromJson(json['search'] as Map<String, dynamic>),
@@ -966,7 +967,7 @@ _$BundleResponse _$_$BundleResponseFromJson(Map<String, dynamic> json) {
     lastModified: json['lastModified'] == null
         ? null
         : Instant.fromJson(json['lastModified'] as String),
-    outcome: json['outcome'],
+    outcome: json['outcome'] == null ? null : ResourceFromJson(json['outcome']),
   );
 }
 
