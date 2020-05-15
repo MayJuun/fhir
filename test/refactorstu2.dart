e88026cb6004520fdb774d7047d27481a6cbd712
Dokotela <jgfaulkenberry@gmail.com>
2020-05-15 00:09:07 -0400

    Closing in

--- test/refactorstu2.dart
+++ test/refactorstu2.dart
@@ -4,18 +4,27 @@ void main() async {
   var tempDir = Directory('./lib/dstu2/domain_resource');
 
   var exp = RegExp(r'\(\{(\n.*)*\}\);\n\n.*factory');
+  var replace = false;
 
   tempDir.list(recursive: true, followLinks: false).listen(
     (FileSystemEntity entity) async {
       if (entity.path.toString().contains('.dart')) {
         var text = await File(entity.path.toString()).readAsString();
-        text = text.replaceAll(' \{', ' ({');
+        for (var i = 1; i < text.length; i++) {
+          if (text[i] == '{' && text[i - 1] == '(') replace = true;
+          if (text[i] == ')' && text[i - 1] == '}') replace = false;
+          if (text[i] == ';' && replace) {
+            text = text.replaceRange(i, i + 1, ',');
+          }
+        }
+        // print(text);
+
         // var pieces = text.split('class ');
         // var fileText = '';
         // pieces.forEach((element) {
         //   if (element.contains(' ')) {
         //     var begin = element.substring(0, element.indexOf(' ')) + '\\(\\{';
-        //     var exp = RegExp('$begin(\n.*)*\\}\\);\n\n.*factory');
+        //
 
         //     element = element.replaceFirst(exp,
         //         '}) = ${element.substring(0, element.indexOf(' '))};\n\nfactory');
