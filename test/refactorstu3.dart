78c3eb2507f2065c61e93e72c3c9fcd57f4c9a4c
Dokotela <jgfaulkenberry@gmail.com>
2020-05-16 16:44:46 -0400

    Nope, that didn't work.

--- test/refactorstu3.dart
+++ test/refactorstu3.dart
@@ -11,9 +11,11 @@ void main() async {
         for (var match in reg.allMatches(oldText)) {
           var name = match.group(0).split(' ')[1];
           var regexp = RegExp('$name\\(\\{(\\n.*)*factory\s$name.fromJson');
-          oldText = oldText.replaceFirst(regexp, '}) = $name;\n\nfactory $name.fromJson');
-          await File(entity.path.toString()).writeAsString(oldText);
+          oldText = oldText.replaceFirst(
+              regexp, '}) = $name;\n\nfactory $name.fromJson');
         }
+        await File(entity.path.toString()).writeAsString(oldText);
+
       }
     },
   );
