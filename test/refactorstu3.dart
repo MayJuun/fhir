d2d45c9be20bd3c8526e7db5f447a23b54a471a3
Dokotela <jgfaulkenberry@gmail.com>
2020-05-16 16:55:38 -0400

    If this works, I just need to change some semicolons

--- test/refactorstu3.dart
+++ test/refactorstu3.dart
@@ -10,12 +10,11 @@ void main() async {
         var oldText = await File(entity.path.toString()).readAsString();
         for (var match in reg.allMatches(oldText)) {
           var name = match.group(0).split(' ')[1];
-          var regexp = RegExp('$name\\s\\{');
-          // oldText = oldText.replaceFirst(
-          //     regexp, '}) = $name;\n\nfactory $name.fromJson');
+          var regexp = RegExp('class $name\\s\\{');
+          oldText = oldText.replaceFirst(
+              regexp, '@freezed\nabstract class $name with _\$$name {\nfactory $name({');
         }
-        // await File(entity.path.toString()).writeAsString(oldText);
-
+        await File(entity.path.toString()).writeAsString(oldText);
       }
     },
   );
