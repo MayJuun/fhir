484012e7d041204620176932d923b691c9bd5065
Dokotela <jgfaulkenberry@gmail.com>
2020-05-20 02:47:11 -0400

    Need to adjust enums and code gen tomorrow

--- lib/stu3/general_types/general_types.enums.dart
+++ lib/stu3/general_types/general_types.enums.dart
@@ -1,1 +1,152 @@
+import 'package:freezed_annotation/freezed_annotation.dart';
 
+enum IdentifierUse {
+  @JsonValue('usual')
+  usual,
+  @JsonValue('official')
+  official,
+  @JsonValue('temp')
+  temp,
+  @JsonValue('secondary')
+  secondary,
+  @JsonValue('unknown')
+  unknown,
+}
+
+// enum QuantityComparator{
+// @JsonValue('<')
+// <,@JsonValue('<=')
+// <=,@JsonValue('>=')
+// >=,@JsonValue('>')
+// >,@JsonValue('unknown')
+// unknown,}
+
+enum HumanNameUse {
+  @JsonValue('usual')
+  usual,
+  @JsonValue('official')
+  official,
+  @JsonValue('temp')
+  temp,
+  @JsonValue('nickname')
+  nickname,
+  @JsonValue('anonymous')
+  anonymous,
+  @JsonValue('old')
+  old,
+  @JsonValue('maiden')
+  maiden,
+  @JsonValue('unknown')
+  unknown,
+}
+
+enum AddressUse {
+  @JsonValue('home')
+  home,
+  @JsonValue('work')
+  work,
+  @JsonValue('temp')
+  temp,
+  @JsonValue('old')
+  old,
+  @JsonValue('unknown')
+  unknown,
+}
+
+enum AddressType {
+  @JsonValue('postal')
+  postal,
+  @JsonValue('physical')
+  physical,
+  @JsonValue('both')
+  both,
+  @JsonValue('unknown')
+  unknown,
+}
+
+enum ContactPointSystem {
+  @JsonValue('phone')
+  phone,
+  @JsonValue('fax')
+  fax,
+  @JsonValue('email')
+  email,
+  @JsonValue('pager')
+  pager,
+  @JsonValue('url')
+  url,
+  @JsonValue('sms')
+  sms,
+  @JsonValue('other')
+  other,
+  @JsonValue('unknown')
+  unknown,
+}
+
+enum ContactPointUse {
+  @JsonValue('home')
+  home,
+  @JsonValue('work')
+  work,
+  @JsonValue('temp')
+  temp,
+  @JsonValue('old')
+  old,
+  @JsonValue('mobile')
+  mobile,
+  @JsonValue('unknown')
+  unknown,
+}
+
+enum TimingRepeat {
+  @JsonValue('s')
+  s,
+  @JsonValue('min')
+  min,
+  @JsonValue('h')
+  h,
+  @JsonValue('d')
+  d,
+  @JsonValue('wk')
+  wk,
+  @JsonValue('mo')
+  mo,
+  @JsonValue('a')
+  a,
+  @JsonValue('unknown')
+  unknown,
+}
+
+// enum TimingRepeat{
+// @JsonValue('s')
+// s,@JsonValue('min')
+// min,@JsonValue('h')
+// h,@JsonValue('d')
+// d,@JsonValue('wk')
+// wk,@JsonValue('mo')
+// mo,@JsonValue('a')
+// a,@JsonValue('unknown')
+// unknown,}
+
+// enum TimingRepeat{
+// @JsonValue('MORN')
+// morn,@JsonValue('AFT')
+// aft,@JsonValue('EVE')
+// eve,@JsonValue('NIGHT')
+// night,@JsonValue('PHS')
+// phs,@JsonValue('HS')
+// hs,@JsonValue('WAKE')
+// wake,@JsonValue('C')
+// c,@JsonValue('CM')
+// cm,@JsonValue('CD')
+// cd,@JsonValue('CV')
+// cv,@JsonValue('AC')
+// ac,@JsonValue('ACM')
+// acm,@JsonValue('ACD')
+// acd,@JsonValue('ACV')
+// acv,@JsonValue('PC')
+// pc,@JsonValue('PCM')
+// pcm,@JsonValue('PCD')
+// pcd,@JsonValue('PCV')
+// pcv,@JsonValue('unknown')
+// unknown,}
--- lib/stu3/special_types/special_types.enums.dart
+++ lib/stu3/special_types/special_types.enums.dart
@@ -1,1 +1,99 @@
+import 'package:freezed_annotation/freezed_annotation.dart';
 
+enum NarrativeStatus {
+  @JsonValue('generated')
+  generated,
+  @JsonValue('extensions')
+  extensions,
+  @JsonValue('additional')
+  additional,
+  @JsonValue('empty')
+  empty,
+  @JsonValue('unknown')
+  unknown,
+}
+
+enum ElementDefinitionRepresentation {
+  @JsonValue('xmlAttr')
+  xmlattr,
+  @JsonValue('xmlText')
+  xmltext,
+  @JsonValue('typeAttr')
+  typeattr,
+  @JsonValue('cdaText')
+  cdatext,
+  @JsonValue('xhtml')
+  xhtml,
+  @JsonValue('unknown')
+  unknown,
+}
+
+enum ElementDefinitionSlicing {
+  @JsonValue('closed')
+  closed,
+  @JsonValue('open')
+  open,
+  @JsonValue('openAtEnd')
+  openatend,
+  @JsonValue('unknown')
+  unknown,
+}
+
+enum ElementDefinitionDiscriminator {
+  @JsonValue('value')
+  value,
+  @JsonValue('exists')
+  exists,
+  @JsonValue('pattern')
+  pattern,
+  @JsonValue('type')
+  type,
+  @JsonValue('profile')
+  profile,
+  @JsonValue('unknown')
+  unknown,
+}
+
+enum ElementDefinitionType {
+  @JsonValue('contained')
+  contained,
+  @JsonValue('referenced')
+  referenced,
+  @JsonValue('bundled')
+  bundled,
+  @JsonValue('unknown')
+  unknown,
+}
+
+enum ElementDefinitionType {
+  @JsonValue('either')
+  either,
+  @JsonValue('independent')
+  independent,
+  @JsonValue('specific')
+  specific,
+  @JsonValue('unknown')
+  unknown,
+}
+
+enum ElementDefinitionConstraint {
+  @JsonValue('error')
+  error,
+  @JsonValue('warning')
+  warning,
+  @JsonValue('unknown')
+  unknown,
+}
+
+enum ElementDefinitionBinding {
+  @JsonValue('required')
+  required,
+  @JsonValue('extensible')
+  extensible,
+  @JsonValue('preferred')
+  preferred,
+  @JsonValue('example')
+  example,
+  @JsonValue('unknown')
+  unknown,
+}
