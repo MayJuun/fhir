ab9ec884da7c93511878f004b71d7d074467a755
Dokotela <jgfaulkenberry@gmail.com>
2020-05-30 17:02:48 -0400

    May need to rerun codegen to include enums in exports

--- lib/r4/resources.dart
+++ lib/r4/resources.dart
@@ -1,6 +1,10 @@
 export 'resource/resource.dart';
 export 'draft_types/draft_types.dart';
 export 'general_types/general_types.dart';
+export 'general_types/general_types.enums.dart';
 export 'metadata_types/metadata_types.dart';
+export 'metadata_types/metadata_types.enums.dart';
 export 'resource_types/resource_types.dart';
+export 'resource_types/resource_types.enums.dart';
 export 'special_types/special_types.dart';
+export 'special_types/special_types.enums.dart';
