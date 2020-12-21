050246621de099be032b8815c9f6a33372d8af7b
Dokotela <jgfaulkenberry@gmail.com>
2020-12-21 13:42:49 -0500

    Attempting to add yaml

---
+++ test/temp.dart
@@ -0,0 +1,36 @@
+import 'dart:convert';
+
+import 'package:fhir/r4.dart';
+import 'package:json2yaml/json2yaml.dart';
+import 'package:yaml/yaml.dart';
+
+void main() {
+  final jsonString = '''{
+  "resourceType": "Patient",
+  "name": 
+  [
+    {
+      "given": 
+      	["Grey"],
+      "family": "Faulkenberry"
+    }
+  ]
+}''';
+  final yamlString = '''resourceType: Patient
+name:
+- given:
+  - Grey
+  family: Faulkenberry''';
+  var pat1 = Resource.fromYaml(yamlString);
+  print(pat1.toJson());
+  var pat2 = Resource.fromYaml(loadYaml(yamlString));
+  print(pat2.toJson());
+  print(pat2.toYamlString());
+  print(pat2.toYamlMap());
+  // var pat2 = Patient.fromJson(jsonDecode(jsonString));
+  // print(pat1 == pat2);
+  // print(json2yaml(pat2.toJson()));
+}
+
+Patient fromYaml(YamlMap yamlString) =>
+    Patient.fromJson(jsonDecode(jsonEncode(yamlString)));
