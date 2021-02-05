a98ad8af6a769e30632fbb160e8b05c1d508688d
Dokotela <jgfaulkenberry@gmail.com>
2021-02-05 07:58:17 -0500

    Checking if ndjson is as easy to parse as it seems like it might be

---
+++ fhir_bulk/lib/main.dart
@@ -0,0 +1,23 @@
+import 'dart:convert';
+import 'dart:io';
+
+import 'package:fhir/r4.dart';
+
+Future<void> main() async {
+  int i = 0;
+  final dir = Directory('./lib/examples-ndjson');
+  for (var file in await dir.list().toList()) {
+    var contents = await File(file.path).readAsString();
+    var contentList = contents.split('\n');
+    for (var resource in contentList) {
+      i++;
+      if (i > 200) break;
+      var resourceFile = Resource.fromJson(json.decode(resource));
+      if (resourceFile.id.isValid) {
+        await File(
+                './lib/examples/${resourceFile.resourceTypeString().toLowerCase()}_${resourceFile.id}.json')
+            .writeAsString(json.encode(resourceFile.toJson()));
+      }
+    }
+  }
+}
