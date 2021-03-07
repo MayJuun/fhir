e8c042ca44547dbee363ed42b7505bd0e6d07ebd
Tilo C <github@legentix.com>
2021-03-07 15:41:05 +0100

    Better test output when JSON parsing fails.

--- fhir/test/validation/r4_validation.dart
+++ fhir/test/validation/r4_validation.dart
@@ -5,22 +5,26 @@ Future<List<String>> r4Validation() async {
   var string = <String>[];
   for (var file in await dir.list().toList()) {
     var contents = await File(file.path).readAsString();
-    var resource = r4.Resource.fromJson(jsonDecode(contents));
-    var result = await checkMapEquality(
+    var resource;
+    try {
+      resource = r4.Resource.fromJson(jsonDecode(contents));
+    } catch (e) {
+      string.add(
+          'JSON example file ${file.path} could not be parsed: ${e.toString()}');
+      continue;
+    }
+    var resultDecode = await checkMapEquality(
         jsonDecode(contents), resource.toJson(), file.toString());
-    if (result != '') {
-      string.add(result);
+    if (resultDecode != '') {
+      string.add(resultDecode);
     }
-  }
-  for (var file in await dir.list().toList()) {
-    var contents = await File(file.path).readAsString();
-    var resource = r4.Resource.fromJson(jsonDecode(contents));
-    var result = await checkMapEquality(
+    var resultToJson = await checkMapEquality(
         resource.toJson(), jsonDecode(contents), file.toString());
-    if (result != '') {
-      string.add(result);
+    if (resultToJson != '') {
+      string.add(resultToJson);
     }
   }
+
   return string;
 }
 
@@ -29,22 +33,26 @@ Future<List<String>> r4ValidationYaml() async {
   var string = <String>[];
   for (var file in await dir.list().toList()) {
     var contents = await File(file.path).readAsString();
-    final tempResource = r4.Resource.fromJson(jsonDecode(contents));
-    var resource = r4.Resource.fromYaml(tempResource.toYaml());
-    var result = await checkMapEquality(
-        jsonDecode(contents), resource.toJson(), file.toString());
-    if (result != '') {
-      string.add(result);
+    var tempResource;
+    try {
+      tempResource = r4.Resource.fromJson(jsonDecode(contents));
+    } catch (e) {
+      string.add(
+          'JSON example file ${file.path} could not be parsed: ${e.toString()}');
+      continue;
     }
-  }
-  for (var file in await dir.list().toList()) {
-    var contents = await File(file.path).readAsString();
-    final tempResource = r4.Resource.fromJson(jsonDecode(contents));
+
     var resource = r4.Resource.fromYaml(tempResource.toYaml());
-    var result = await checkMapEquality(
+
+    var resultDecode = await checkMapEquality(
+        jsonDecode(contents), resource.toJson(), file.toString());
+    if (resultDecode != '') {
+      string.add(resultDecode);
+    }
+    var resultToJson = await checkMapEquality(
         resource.toJson(), jsonDecode(contents), file.toString());
-    if (result != '') {
-      string.add(result);
+    if (resultToJson != '') {
+      string.add(resultToJson);
     }
   }
   return string;
