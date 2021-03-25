5e06a6ed5480481ffabd395bac807a6209e79d43
Dokotela <jgfaulkenberry@gmail.com>
2021-03-24 22:53:28 -0400

    Not sure why I can't login directly anymore

--- fhir_auth/example/lib/gcs_request.dart
+++ fhir_auth/example/lib/gcs_request.dart
@@ -21,37 +21,37 @@ Future gcsRequest({
     print(e);
   }
 
-  final _newPatient = newPatient();
-  print('Patient to be uploaded: ${_newPatient.toJson()}');
-  final request1 = FhirRequest.create(
-    base: client.baseUrl.value ?? Uri.parse('127.0.0.1'),
-    resource: _newPatient,
-  );
+  // final _newPatient = newPatient();
+  // print('Patient to be uploaded: ${_newPatient.toJson()}');
+  // final request1 = FhirRequest.create(
+  //   base: client.baseUrl.value ?? Uri.parse('127.0.0.1'),
+  //   resource: _newPatient,
+  // );
 
-  print(await client.authHeaders);
+  // print(await client.authHeaders);
 
-  Id? newId;
-  try {
-    final response = await request1.request(headers: await client.authHeaders);
-    newId = response?.id;
-    print('Uploaded patient: ${response?.toJson()}');
-  } catch (e) {
-    print(e);
-  }
-  if (newId is! Id) {
-    print(newId);
-  } else {
-    final request2 = FhirRequest.read(
-      base: client.baseUrl.value ?? Uri.parse('127.0.0.1'),
-      type: R4ResourceType.Patient,
-      id: newId,
-    );
-    try {
-      final response2 =
-          await request2.request(headers: await client.authHeaders);
-      print('Uploaded patient: ${response2?.toJson()}');
-    } catch (e) {
-      print(e);
-    }
-  }
+  // Id? newId;
+  // try {
+  //   final response = await request1.request(headers: await client.authHeaders);
+  //   newId = response?.id;
+  //   print('Uploaded patient: ${response?.toJson()}');
+  // } catch (e) {
+  //   print(e);
+  // }
+  // if (newId is! Id) {
+  //   print(newId);
+  // } else {
+  //   final request2 = FhirRequest.read(
+  //     base: client.baseUrl.value ?? Uri.parse('127.0.0.1'),
+  //     type: R4ResourceType.Patient,
+  //     id: newId,
+  //   );
+  //   try {
+  //     final response2 =
+  //         await request2.request(headers: await client.authHeaders);
+  //     print('Uploaded patient: ${response2?.toJson()}');
+  //   } catch (e) {
+  //     print(e);
+  //   }
+  // }
 }
