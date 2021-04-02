5baaf778113ce2e608106e2ed3d5e60a7df117ed
Dokotela <jgfaulkenberry@gmail.com>
2021-04-02 15:32:31 -0400

    Can get code, but has errors

--- fhir_auth_web/lib/main.dart
+++ fhir_auth_web/lib/main.dart
@@ -1,37 +1,53 @@
+// ignore: avoid_web_libraries_in_flutter
 import 'dart:html' as html;
 import 'package:flutter/material.dart';
 import 'package:oauth2/oauth2.dart' as oauth2;
 
 import 'api.dart';
 
-final _redirect = 'http://localhost:39211/redirect/';
+final _redirect = 'http://localhost:37801/redirect/';
 void main() => runApp(MyApp());
 
-class MyApp extends StatelessWidget with WidgetsBindingObserver {
+class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     final base = Uri.base.toString();
     print(base);
-    if (base.contains('code=') && base.contains('redirect')) {
-      return MaterialApp(
-          home:
-              Scaffold(body: Text('Successful Auth, here\'s the url: $base')));
-    } else {
-      var grant = oauth2.AuthorizationCodeGrant(
-        Api.aidboxClientId,
-        Uri.parse('https://fhirfli.aidbox.app/auth/authorize'),
-        Uri.parse('https://fhirfli.aidbox.app/auth/token'),
-        secret: Api.aidboxSecret,
-      );
-      var authorizationUrl = grant.getAuthorizationUrl(Uri.parse(_redirect),
-          scopes: ['openid', 'offlineAccess']);
+    return base.contains('code=') && base.contains('redirect')
+        ? MainPage()
+        : AuthPage();
+  }
+}
 
-      WidgetsBinding.instance!.addPostFrameCallback((_) {
-        html.window.location.assign(authorizationUrl.toString());
-      });
+class AuthPage extends StatefulWidget {
+  @override
+  _AuthPageState createState() => _AuthPageState();
+}
 
-      return MaterialApp(
-          home: Scaffold(body: Text('Please wait while we log you in')));
-    }
+class _AuthPageState extends State<AuthPage> with WidgetsBindingObserver {
+  @override
+  void initState() {
+    var grant = oauth2.AuthorizationCodeGrant(
+      Api.aidboxClientId,
+      Uri.parse('https://fhirfli.aidbox.app/auth/authorize'),
+      Uri.parse('https://fhirfli.aidbox.app/auth/token'),
+      secret: Api.aidboxSecret,
+    );
+    var authorizationUrl = grant.getAuthorizationUrl(Uri.parse(_redirect),
+        scopes: ['openid', 'offlineAccess']);
+    html.window.location.assign(authorizationUrl.toString());
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) => MaterialApp(
+      home: Scaffold(body: Text('Please wait while we log you in')));
+}
+
+class MainPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+        home: Scaffold(body: Text('Successful code: ${Uri.base}')));
   }
 }
