15780521d736b7fedcec6b1ac3444ca81b237db5
Dokotela <jgfaulkenberry@gmail.com>
2021-05-04 22:55:17 -0400

    Closer still

--- fhir_path/fhir_path/main.dart
+++ fhir_path/fhir_path/main.dart
@@ -1,95 +1,177 @@
 void main() {
-  var listOfLists = ['0: '];
+  var initialList = ['0: '];
   var testString =
-      "Patient.telecom.exists(system = 'phone' and use = 'mobile')";
+      "Patient.telecom.exists(system = 'phone' or use = 'mobile' and use = 'mobile' and use = 'landline')";
   var depth = 0;
   for (var i = 0; i < testString.length; i++) {
     if (testString[i] == ('(')) {
+      var tempString = testString.substring(0, i);
+      var index =
+          functions.indexWhere((element) => tempString.endsWith(element));
+      if (index != -1) {
+        initialList.last = initialList.last
+            .substring(0, initialList.last.length - functions[index].length);
+      }
       depth++;
-      listOfLists.add('$depth: (');
+      initialList.add('$depth: ${index != -1 ? functions[index] : ""}(');
+      depth++;
+      initialList.add('$depth: ');
     } else if ((testString[i] == ')')) {
-      listOfLists.last += ')';
       depth--;
-      listOfLists.add('$depth: ');
+      initialList.add('$depth: )');
+      depth--;
+      initialList.add('$depth: ');
     } else {
-      listOfLists.last += testString[i];
+      initialList.last += testString[i];
     }
   }
-  listOfLists.forEach((element) {
+  var finalList = <String>[];
+  for (var list in initialList) {
+    final number = list.substring(0, 2);
+    final restOfList = list.substring(3, list.length);
+    final tempList = restOfList.split(' and ');
+    if (tempList.length > 1) {
+      tempList.forEach((v) {
+        finalList.add('$number$v');
+        if (tempList.indexOf(v) < tempList.length - 1) {
+          finalList.add('${number}and');
+        }
+      });
+    } else {
+      finalList.add('$number${tempList.join('')}');
+    }
+  }
+  initialList.clear();
+  initialList.addAll(finalList);
+  finalList.clear();
+  for (var list in initialList) {
+    final number = list.substring(0, 2);
+
+    final restOfList = list.substring(3, list.length);
+    final tempList = restOfList.split(' or ');
+    if (tempList.length > 1) {
+      tempList.forEach((v) {
+        finalList.add('$number$v');
+        if (tempList.indexOf(v) < tempList.length - 1) {
+          finalList.add('${number}or');
+        }
+      });
+    } else {
+      finalList.add('$number${tempList.join('')}');
+    }
+  }
+  initialList.forEach((element) {
     print('${" " * int.parse(element[0]) * 3}$element');
   });
 }
 
-var functions = [
-  '.empty(',
-  '.exists(',
-  '.all(',
-  '.allTrue(',
-  '.anyTrue(',
-  '.allFalse(',
-  '.anyFalse(',
-  '.subsetOf(',
-  '.supersetOf(',
-  '.count(',
-  '.distinct(',
-  '.isDistinct(',
-  '.where(',
-  '.select(',
-  '.repeat(',
-  '.ofType(',
-  '.single(',
-  '.first(',
-  '.last(',
-  '.tail(',
-  '.skip(',
-  '.take(',
-  '.intersect(',
-  '.exclude(',
-  '.union(',
-  '.combine(',
-  '.iif(',
-  '.toBoolean(',
-  '.convertsToBoolean(',
-  '.toInteger(',
-  '.convertsToInteger(',
-  '.toDate(',
-  '.convertsToDate(',
-  '.toDateTime(',
-  '.convertsToDateTime(',
-  '.toDecimal(',
-  '.convertsToDecimal(',
-  '.toQuantity(',
-  '.convertsToQuantity(',
-  '.toString(',
-  '.convertsToString(',
-  '.toTime(',
-  '.convertsToTime(',
-  '.indexOf(',
-  '.substring(',
-  '.startsWith(',
-  '.endsWith(',
-  '.contains(',
-  '.upper(',
-  '.lower(',
-  '.replace(',
-  '.matches(',
-  '.replaceMatches(',
-  '.length(',
-  '.toChars(',
-  '.abs(',
-  '.ceiling(',
-  '.exp(',
-  '.floor(',
-  '.ln(',
-  '.log(',
-  '.power(',
-  '.round(',
-  '.sqrt(',
-  '.truncate(',
-  '.children(',
-  '.descendants(',
-  '.trace(',
-  '.now(',
-  '.timeOfDay(',
-  '.today(',
+const functions = [
+  '.empty',
+  '.exists',
+  '.all',
+  '.allTrue',
+  '.anyTrue',
+  '.allFalse',
+  '.anyFalse',
+  '.subsetOf',
+  '.supersetOf',
+  '.count',
+  '.distinct',
+  '.isDistinct',
+  '.where',
+  '.select',
+  '.repeat',
+  '.ofType',
+  '.single',
+  '.first',
+  '.last',
+  '.tail',
+  '.skip',
+  '.take',
+  '.intersect',
+  '.exclude',
+  '.union',
+  '.combine',
+  '.iif',
+  '.toBoolean',
+  '.convertsToBoolean',
+  '.toInteger',
+  '.convertsToInteger',
+  '.toDate',
+  '.convertsToDate',
+  '.toDateTime',
+  '.convertsToDateTime',
+  '.toDecimal',
+  '.convertsToDecimal',
+  '.toQuantity',
+  '.convertsToQuantity',
+  '.toString',
+  '.convertsToString',
+  '.toTime',
+  '.convertsToTime',
+  '.indexOf',
+  '.substring',
+  '.startsWith',
+  '.endsWith',
+  '.contains',
+  '.upper',
+  '.lower',
+  '.replace',
+  '.matches',
+  '.replaceMatches',
+  '.length',
+  '.toChars',
+  '.abs',
+  '.ceiling',
+  '.exp',
+  '.floor',
+  '.ln',
+  '.log',
+  '.power',
+  '.round',
+  '.sqrt',
+  '.truncate',
+  '.children',
+  '.descendants',
+  '.trace',
+  '.now',
+  '.timeOfDay',
+  '.today',
+  '.not',
+];
+
+const andOrXor = [
+  ' and ',
+  ' or ',
+  ' xor ',
+];
+
+const operations = [
+  ' = ',
+  ' ~ ',
+  ' != ',
+  ' !~ ',
+  ' > ',
+  ' < ',
+  ' >= ',
+  ' <= ',
+  ' is ',
+  ' as ',
+  ' | ',
+  ' in ',
+  ' contains ',
+  ' implies ',
+  ' * ',
+  ' × ',
+  ' / ',
+  ' ÷ ',
+  ' + ',
+  ' - ',
+  ' − ',
+  ' div ',
+  ' mod ',
+  ' & ',
+  // '-',
+  // '+',
+  // '−',
 ];
