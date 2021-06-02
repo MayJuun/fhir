de3a54418bf6b5a66cbdb679a3f5d1fed016e7c0
Dokotela <jgfaulkenberry@gmail.com>
2021-06-02 18:57:01 -0400

    Moved all fhir tests to own folder (made fhir package to large to upload)

--- fhir_path/petit_parser/main.dart
+++ fhir_path/petit_parser/main.dart
@@ -1,9 +1,10 @@
 import 'package:petitparser/petitparser.dart';
 
 void main() {
-  final jsonPath = selector.star();
+  final definition = FhirPathDefinition();
+  final parser = definition.build();
   for (var s in testStrings) {
-    print(jsonPath.parse(s).value);
+    print(parser.parse(s).value);
   }
 }
 
@@ -19,14 +20,39 @@ final testStrings = [
   '`us-zip`',
 ];
 
+class FhirPathDefinition extends GrammarDefinition {
+  Parser start() => ref0(term).end();
+  Parser term() => (ref0(paren) | ref0(field)).plus().map((v) => v);
+
+  Parser paren() => (char('(') & ref0(identifier)) & char(')').map((v) => v);
+  Parser field() => (ref0(path) |
+          ref0(simpleIdentifier) |
+          // ref0(delimitedIdentifier) |
+          ref0(operation))
+      .map((v) => v);
+
+  Parser operation() =>
+      (ref0(field) & string(' = ') & ref0(field)).map((v) => v);
+  Parser identifier() => (ref0(simpleIdentifier)
+      // | ref0(delimitedIdentifier)
+      )
+      .map((v) => v);
+  Parser path() => (char('.') & ref0(identifier)).map((v) => v);
+
+  Parser simpleIdentifier() =>
+      (word() | char('_')).plus().flatten().map((v) => v);
+  // Parser delimitedIdentifier() =>
+  //     (char('`') & any().plus() & char('`')).map((v) => v);
+}
+
 /// ToDo: currently doesn't work properly
-final delimitedIdentifier =
-    (char('`') & any().plus() & char('`')).map((v) => v);
-final simpleIdentifier = (word() | char('_')).plus().flatten().map((v) => v);
-final path = (char('.') & identifier).map((v) => v);
-final identifier = (simpleIdentifier | delimitedIdentifier).map((v) => v);
-final operation = (field & string(' = ') & field).map((v) => v);
-final field =
-    (path | simpleIdentifier | delimitedIdentifier | operation).map((v) => v);
-final paren = (char('(') & identifier) & char(')').map((v) => v);
-final selector = paren | field;
+// final delimitedIdentifier =
+//     (char('`') & any().plus() & char('`')).map((v) => v);
+// final simpleIdentifier = (word() | char('_')).plus().flatten().map((v) => v);
+// final path = (char('.') & identifier).map((v) => v);
+// final identifier = (simpleIdentifier | delimitedIdentifier).map((v) => v);
+// final operation = (field & string(' = ') & field).map((v) => v);
+// final field =
+//     (path | simpleIdentifier | delimitedIdentifier | operation).map((v) => v);
+// final paren = (char('(') & identifier) & char(')').map((v) => v);
+// final selector = paren | field;
