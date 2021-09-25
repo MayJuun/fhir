e4039e1ea2f62fd2928058a57881b5ce6549a1f2
Dokotela <jgfaulkenberry@gmail.com>
2021-09-25 18:57:29 -0400

    Anyone else want to take auth away from me?

--- fhir_auth/mobileauthdemo/lib/smart_request.dart
+++ fhir_auth/mobileauthdemo/lib/smart_request.dart
@@ -17,44 +17,43 @@ Future smartRequest() async {
 
   try {
     await client.initialize();
-  } catch (e, stack) {
-    print('Error $e');
-    print('Stack $stack');
-  }
-
-  if (client.fhirUri?.value != null) {
-    final _newPatient = newPatient();
-    print('Patient to be uploaded:\n${_newPatient.toJson()}');
-    final request1 = FhirRequest.create(
-      base: client.fhirUri!.value!,
-      //?? Uri.parse('127.0.0.1'),
-      resource: _newPatient,
-      fhirClient: client,
-    );
-
-    Id? newId;
-    try {
-      final response = await request1.request();
-      print('Response from upload:\n${response?.toJson()}');
-      newId = response?.id;
-    } catch (e) {
-      print(e);
-    }
-    if (newId is! Id) {
-      print(newId);
-    } else {
-      final request2 = FhirRequest.read(
-        base: client.fhirUri!.value ?? Uri.parse('127.0.0.1'),
-        type: R4ResourceType.Patient,
-        id: newId,
+    if (client.fhirUri?.value != null) {
+      final _newPatient = newPatient();
+      print('Patient to be uploaded:\n${_newPatient.toJson()}');
+      final request1 = FhirRequest.create(
+        base: client.fhirUri!.value!,
+        //?? Uri.parse('127.0.0.1'),
+        resource: _newPatient,
         fhirClient: client,
       );
+
+      Id? newId;
       try {
-        final response = await request2.request();
-        print('Response from read:\n${response?.toJson()}');
+        final response = await request1.request();
+        print('Response from upload:\n${response?.toJson()}');
+        newId = response?.id;
       } catch (e) {
         print(e);
       }
+      if (newId is! Id) {
+        print(newId);
+      } else {
+        final request2 = FhirRequest.read(
+          base: client.fhirUri!.value ?? Uri.parse('127.0.0.1'),
+          type: R4ResourceType.Patient,
+          id: newId,
+          fhirClient: client,
+        );
+        try {
+          final response = await request2.request();
+          print('Response from read:\n${response?.toJson()}');
+        } catch (e) {
+          print(e);
+        }
+      }
     }
+  } catch (e, stack) {
+    print('Error $e');
+    print('Stack $stack');
   }
 }
