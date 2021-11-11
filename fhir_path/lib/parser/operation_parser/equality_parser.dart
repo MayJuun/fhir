33df15a5653afb96615073b30b9b06da7950be5d
Tilo <tiloc@users.noreply.github.com>
2021-11-11 12:31:47 +0100

    - Fix '!=' (NotEqualsParser).

--- fhir_path/lib/parser/operation_parser/equality_parser.dart
+++ fhir_path/lib/parser/operation_parser/equality_parser.dart
@@ -33,37 +33,18 @@ class EquivalentParser extends OperatorParser {
   }
 }
 
+/// https://hl7.org/fhirpath/#not-equals
+///
+/// A != B is short-hand for (A = B).not()
 class NotEqualsParser extends OperatorParser {
   NotEqualsParser();
-  ParserList before = ParserList([]);
-  ParserList after = ParserList([]);
+
   List execute(List results, Map passed) {
-    final executedBefore =
-        before.length == 1 && before.first is IdentifierParser
-            ? [(before.first as IdentifierParser).value]
-            : before.execute(results.toList(), passed);
-    final executedAfter = after.length == 1 && after.first is IdentifierParser
-        ? [(after.first as IdentifierParser).value]
-        : after.execute(results.toList(), passed);
-    if (executedBefore.isEmpty || executedAfter.isEmpty) {
-      return [];
-    } else {
-      for (var i = 0; i < executedBefore.length; i++) {
-        if ((executedBefore[i] is String ||
-                    executedBefore[i] is bool ||
-                    executedBefore[i] is num
-                ? executedBefore[i]
-                : executedBefore[i].value) ==
-            (executedAfter[i] is String ||
-                    executedAfter[i] is bool ||
-                    executedAfter[i] is num
-                ? executedAfter[i]
-                : executedAfter[i].value)) {
-          return [false];
-        }
-      }
-      return [true];
-    }
+    final equalsParser = EqualsParser();
+    equalsParser.before = this.before;
+    equalsParser.after = this.after;
+    final equality = equalsParser.execute(results, passed);
+    return FpNotParser().execute(equality, passed);
   }
 }
 
