a677bb9d9ded3c5f18c3b0f4ecc0b4a94057f937
Dokotela <grey.faulkenberry@mayjuun.com>
2021-12-05 20:53:52 -0500

    other bits of the update for auth

--- fhir_auth/lib/enums/enums.dart
+++ fhir_auth/lib/enums/enums.dart
@@ -1,5 +1,6 @@
 export 'compartment.dart';
 export 'enums_to_string.dart';
+export 'mime_type.dart';
 export 'mode.dart';
 export 'restful_request.dart';
 export 'summary.dart';
--- fhir_auth/lib/fhir_client/export.dart
+++ fhir_auth/lib/fhir_client/export.dart
@@ -1,3 +1,2 @@
 export 'fhir_client.dart';
 export 'gcs_client.dart';
-export 'insecure_fhir_client.dart';
