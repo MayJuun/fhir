902d0fe6e6c9f18ac8f9a40b07f37a31dfb79423
Dokotela <grey.faulkenberry@mayjuun.com>
2022-03-01 16:42:38 -0500

    FHIR 0.6.0 validated, README updated

--- fhir/lib/primitive_types/integer64.dart
+++ fhir/lib/primitive_types/integer64.dart
@@ -1,5 +1,4 @@
 import 'dart:convert';
-import 'dart:math';
 
 import 'package:yaml/yaml.dart';
 
@@ -9,17 +8,20 @@ class Integer64 extends FhirNumber {
   const Integer64._(String valueString, int? valueNumber, bool isValid)
       : super(valueString, valueNumber, isValid);
 
+  static const double bit64 = 9223372036854775808;
+
   factory Integer64(dynamic inValue) {
     if (inValue is int) {
-      return inValue <= pow(2, 63) && inValue >= pow(2, 63)
+      return inValue <= bit64 && inValue >= -bit64
           ? Integer64._(inValue.toString(), inValue, true)
           : Integer64._(inValue.toString(), null, false);
     } else if (inValue is num) {
       final int? tempInteger64 = int.tryParse(inValue.toString());
       return tempInteger64 == null
           ? Integer64._(inValue.toString(), null, false)
-          : tempInteger64 <= pow(2, 63) && tempInteger64 >= pow(2, 63)
-              ? Integer64._(inValue.toString(), tempInteger64, true)
+          : tempInteger64 <= bit64 && tempInteger64 >= -bit64
+              ? Integer64._(
+                  inValue.toString(), tempInteger64, true)
               : Integer64._(inValue.toString(), null, false);
     }
     throw ArgumentError('Integer64 cannot be constructed from $inValue.');
