5c39e25ef7a48e22a548747038c5c08fab875baa
Dokotela <grey.faulkenberry@mayjuun.com>
2022-06-29 13:01:25 -0400

    4b works on fhir & fhir_at_rest

--- fhir_test/test/roundtrip_test.dart
+++ fhir_test/test/roundtrip_test.dart
@@ -2,16 +2,14 @@ import 'package:test/test.dart';
 
 import 'validation/validation.dart';
 
-Future roundtripTest() async {
+Future roundtripTestJson() async {
   group(
     'Runs through all examples provided. Each example resource is a Json file,'
-    'we read in that file as a Resource, convert it back to a map, and then '
-    'do a deep comparison of the input map with the output map to test for any '
-    'inconsistencies. For Yaml it is read as a map, converted to Yaml, then a'
-    'resource is created from that Yaml string, and finally back to a map, '
-    'where the same comparisons are done as above. It then reverses them and '
-    'and performs a deep comparison of the output to the input. Any files with '
-    'errors are printed out in the debug console',
+    'we read in that file as a String, convert it to a Map, then create the '
+    'Resource. We then convert it back to Map, and perform a deep comparison '
+    'of the input map with the output map to test for any inconsistencies. '
+    'It then reverses them and and performs a deep comparison of the output '
+    'to the input. Any files with errors are printed out in the debug console',
     () {
       test(
         '\n****Dstu2 Json has been Validated****',
@@ -61,7 +59,19 @@ Future roundtripTest() async {
         },
         timeout: Timeout(Duration(minutes: 15)),
       );
+    },
+  );
+}
 
+Future roundtripTestYaml() async {
+  group(
+    'Runs through all examples provided. Each example resource is a Json file,'
+    'we read in that file as a String, convert it to a Map, Yaml, and finally '
+    'a Resource. We then convert it back to Map, and perform a deep comparison '
+    'of the input map with the output map to test for any inconsistencies. '
+    'It then reverses them and and performs a deep comparison of the output '
+    'to the input. Any files with errors are printed out in the debug console',
+    () {
       test(
         '\n****Dstu2 Yaml has been Validated****',
         () async {
