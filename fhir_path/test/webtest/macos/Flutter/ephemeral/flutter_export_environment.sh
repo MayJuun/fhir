964d26c5ab3814f1214453027869d3d7388ac2fb
John Manning <john.d.manning@gmail.com>
2022-07-18 15:44:21 -0400

    add generated files for fhir_path/test

---
+++ fhir_path/test/webtest/macos/Flutter/ephemeral/flutter_export_environment.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+# This is a generated file; do not edit or check into version control.
+export "FLUTTER_ROOT=/opt/homebrew/Caskroom/flutter/3.0.5/flutter"
+export "FLUTTER_APPLICATION_PATH=/Users/juun/workspace/fhir/fhir_path/test/webtest"
+export "COCOAPODS_PARALLEL_CODE_SIGN=true"
+export "FLUTTER_BUILD_DIR=build"
+export "FLUTTER_BUILD_NAME=1.0.0"
+export "FLUTTER_BUILD_NUMBER=1"
+export "DART_OBFUSCATION=false"
+export "TRACK_WIDGET_CREATION=false"
+export "TREE_SHAKE_ICONS=false"
+export "PACKAGE_CONFIG=.dart_tool/package_config.json"
