1e75286a82d17469118e4e2b6bab3da5d149ffce
Dokotela <grey.faulkenberry@mayjuun.com>
2022-10-03 22:28:15 -0400

    Comments1

--- fhir/lib/r4/special_types/special_types.dart
+++ fhir/lib/r4/special_types/special_types.dart
@@ -13,9 +13,36 @@ part 'special_types.enums.dart';
 part 'special_types.freezed.dart';
 part 'special_types.g.dart';
 
+/// [Narrative] A human-readable summary of the resource conveying the essential
+/// clinical and business information for the resource.
 @freezed
 class Narrative with _$Narrative {
+  /// [Narrative] A human-readable summary of the resource conveying the essential
+  /// clinical and business information for the resource.
+
   Narrative._();
+
+  /// [Narrative] A human-readable summary of the resource conveying the essential
+  /// clinical and business information for the resource.
+  ///
+  /// [id] Unique id for the element within a resource (for internal references).
+  /// This may be any string value that does not contain spaces.
+  ///
+  /// [extension_] May be used to represent additional information that is not
+  /// part of the basic definition of the element. To make the use of extensions
+  /// safe and manageable, there is a strict set of governance  applied to the
+  /// definition and use of extensions. Though any implementer can define an
+  /// extension, there is a set of requirements that SHALL be met as part of the
+  /// definition of the extension.
+  ///
+  /// [status] The status of the narrative - whether it\u0027s entirely generated
+  /// (from just the defined data or the extensions too), or whether a human
+  /// authored it and it may contain additional data.
+  ///
+  /// [statusElement] (_status) Extensions for status
+  ///
+  /// [div] The actual narrative content, a stripped down version of XHTML.
+
   factory Narrative({
     String? id,
     @JsonKey(name: 'extension') List<FhirExtension>? extension_,
@@ -55,9 +82,35 @@ class Narrative with _$Narrative {
   }
 }
 
+/// [CodeableReference] A reference to a resource (by instance), or instead, a
+/// reference to a concept defined in a terminology or ontology (by class).
+
 @freezed
 class CodeableReference with _$CodeableReference {
+  /// [CodeableReference] A reference to a resource (by instance), or instead, a
+  /// reference to a concept defined in a terminology or ontology (by class).
+
   CodeableReference._();
+
+  /// [CodeableReference] A reference to a resource (by instance), or instead, a
+  /// reference to a concept defined in a terminology or ontology (by class).
+  ///
+  /// [id] Unique id for the element within a resource (for internal references).
+  /// This may be any string value that does not contain spaces.",
+  ///
+  /// [extension_] May be used to represent additional information that is not
+  /// part of the basic definition of the element. To make the use of extensions
+  /// safe and manageable, there is a strict set of governance  applied to the
+  /// definition and use of extensions. Though any implementer can define an
+  /// extension, there is a set of requirements that SHALL be met as part of the
+  /// definition of the extension.
+  ///
+  /// [concept] A reference to a concept - e.g. the information is identified by
+  /// its general class to the degree of precision found in the terminology.",
+  ///
+  /// [reference] A reference to a resource the provides exact details about the
+  /// information being referenced.
+
   factory CodeableReference({
     String? id,
     @JsonKey(name: 'extension') List<FhirExtension>? extension_,
@@ -876,10 +929,6 @@ class ElementDefinition with _$ElementDefinition {
   ///  abstract is false').
   ///
   /// [defaultValueDosage] The value that should be used if there is no value
-  /// stated in the instance (e.g. 'if not otherwise specified, the abstract is
-  ///  false').
-  ///
-  /// [defaultValueMeta] The value that should be used if there is no value
   /// stated in the instance (e.g. 'if not otherwise specified, the abstract is
   ///  false').
   ///
