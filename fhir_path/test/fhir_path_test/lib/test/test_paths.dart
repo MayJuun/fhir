4d703f34a11cce95bbc7d6f3273337408dd77f54
Dokotela <grey.faulkenberry@mayjuun.com>
2022-10-13 19:52:10 -0400

    739 tests passed

--- fhir_path/test/fhir_path_test/lib/test/test_paths.dart
+++ fhir_path/test/fhir_path_test/lib/test/test_paths.dart
@@ -16,90 +16,90 @@ void testPaths() {
         '{family: Smith, given: [John, Jacob, Jingleheimer]}]',
       );
     });
-    // test('Patient Family Name', () {
-    //   expect(
-    //     walkPath('name.family'),
-    //     '[Faulkenberry, Niel, Smith]',
-    //   );
-    // });
-    // test('Patient Given Name', () {
-    //   expect(
-    //     walkPath('name.given'),
-    //     '[Jason, Grey, Kristin, John, Jacob, Jingleheimer]',
-    //   );
-    // });
-    // test('Patient Given Name with ResourceType', () {
-    //   expect(
-    //     walkPath('Patient.name.given'),
-    //     '[Jason, Grey, Kristin, John, Jacob, Jingleheimer]',
-    //   );
-    // });
-    // test('Patient Address Period', () {
-    //   expect(
-    //       walkPath('address.period'),
-    //       '[{extension: [{valueCount: {unit: Mg}}, {valueCount: {unit: mL}}]},'
-    //       ' {extension: [{valueCount: {unit: Kg}}, {valueCount: {unit: Km}}]}, '
-    //       '{extension: [{valueCount: {unit: Feet}}, {valueCount: {unit: inches}}]}]');
-    // });
-    // test('Patient Address Period Extension', () {
-    //   expect(
-    //       walkPath('address.period.extension'),
-    //       '[{valueCount: {unit: Mg}}, '
-    //       '{valueCount: {unit: mL}}, '
-    //       '{valueCount: {unit: Kg}}, '
-    //       '{valueCount: {unit: Km}}, '
-    //       '{valueCount: {unit: Feet}}, '
-    //       '{valueCount: {unit: inches}}]');
-    // });
-    // test('Patient Address Period Extension ValueCount', () {
-    //   expect(
-    //       walkPath('address.period.extension.valueCount'),
-    //       '[{unit: Mg}, '
-    //       '{unit: mL}, '
-    //       '{unit: Kg}, '
-    //       '{unit: Km}, '
-    //       '{unit: Feet}, '
-    //       '{unit: inches}]');
-    // });
-    // test('Patient Address Period Extension ValueCount Unit', () {
-    //   expect(walkPath('address.period.extension.valueCount.unit'),
-    //       '[Mg, mL, Kg, Km, Feet, inches]');
-    // });
-    // test('Sample Patient', () {
-    //   expect(
-    //       walkFhirPath(
-    //           context: patient.toJson(), pathExpression: 'Patient.text.status'),
-    //       ['generated']);
-    //   expect(
-    //       walkFhirPath(
-    //           context: patient.toJson(), pathExpression: 'Patient.text.div'),
-    //       [
-    //         '<div xmlns="http://www.w3.org/1999/xhtml">\n'
-    //             '\t\t\t<table>\n'
-    //             '\t\t\t\t<tbody>\n'
-    //             '\t\t\t\t\t<tr>\n'
-    //             '\t\t\t\t\t\t<td>Name</td>\n'
-    //             '\t\t\t\t\t\t<td>Peter James \n'
-    //             '              <b>Chalmers</b> (&quot;Jim&quot;)\n'
-    //             '            </td>\n'
-    //             '\t\t\t\t\t</tr>\n'
-    //             '\t\t\t\t\t<tr>\n'
-    //             '\t\t\t\t\t\t<td>Address</td>\n'
-    //             '\t\t\t\t\t\t<td>534 Erewhon, Pleasantville, Vic, 3999</td>\n'
-    //             '\t\t\t\t\t</tr>\n'
-    //             '\t\t\t\t\t<tr>\n'
-    //             '\t\t\t\t\t\t<td>Contacts</td>\n'
-    //             '\t\t\t\t\t\t<td>Home: unknown. Work: (03) 5555 6473</td>\n'
-    //             '\t\t\t\t\t</tr>\n'
-    //             '\t\t\t\t\t<tr>\n'
-    //             '\t\t\t\t\t\t<td>Id</td>\n'
-    //             '\t\t\t\t\t\t<td>MRN: 12345 (Acme Healthcare)</td>\n'
-    //             '\t\t\t\t\t</tr>\n'
-    //             '\t\t\t\t</tbody>\n'
-    //             '\t\t\t</table>\n'
-    //             '\t\t</div>'
-    //       ]);
-    // });
+    test('Patient Family Name', () {
+      expect(
+        walkPath('name.family'),
+        '[Faulkenberry, Niel, Smith]',
+      );
+    });
+    test('Patient Given Name', () {
+      expect(
+        walkPath('name.given'),
+        '[Jason, Grey, Kristin, John, Jacob, Jingleheimer]',
+      );
+    });
+    test('Patient Given Name with ResourceType', () {
+      expect(
+        walkPath('Patient.name.given'),
+        '[Jason, Grey, Kristin, John, Jacob, Jingleheimer]',
+      );
+    });
+    test('Patient Address Period', () {
+      expect(
+          walkPath('address.period'),
+          '[{extension: [{valueCount: {unit: Mg}}, {valueCount: {unit: mL}}]},'
+          ' {extension: [{valueCount: {unit: Kg}}, {valueCount: {unit: Km}}]}, '
+          '{extension: [{valueCount: {unit: Feet}}, {valueCount: {unit: inches}}]}]');
+    });
+    test('Patient Address Period Extension', () {
+      expect(
+          walkPath('address.period.extension'),
+          '[{valueCount: {unit: Mg}}, '
+          '{valueCount: {unit: mL}}, '
+          '{valueCount: {unit: Kg}}, '
+          '{valueCount: {unit: Km}}, '
+          '{valueCount: {unit: Feet}}, '
+          '{valueCount: {unit: inches}}]');
+    });
+    test('Patient Address Period Extension ValueCount', () {
+      expect(
+          walkPath('address.period.extension.valueCount'),
+          '[{unit: Mg}, '
+          '{unit: mL}, '
+          '{unit: Kg}, '
+          '{unit: Km}, '
+          '{unit: Feet}, '
+          '{unit: inches}]');
+    });
+    test('Patient Address Period Extension ValueCount Unit', () {
+      expect(walkPath('address.period.extension.valueCount.unit'),
+          '[Mg, mL, Kg, Km, Feet, inches]');
+    });
+    test('Sample Patient', () {
+      expect(
+          walkFhirPath(
+              context: patient.toJson(), pathExpression: 'Patient.text.status'),
+          ['generated']);
+      expect(
+          walkFhirPath(
+              context: patient.toJson(), pathExpression: 'Patient.text.div'),
+          [
+            '<div xmlns="http://www.w3.org/1999/xhtml">\n'
+                '\t\t\t<table>\n'
+                '\t\t\t\t<tbody>\n'
+                '\t\t\t\t\t<tr>\n'
+                '\t\t\t\t\t\t<td>Name</td>\n'
+                '\t\t\t\t\t\t<td>Peter James \n'
+                '              <b>Chalmers</b> (&quot;Jim&quot;)\n'
+                '            </td>\n'
+                '\t\t\t\t\t</tr>\n'
+                '\t\t\t\t\t<tr>\n'
+                '\t\t\t\t\t\t<td>Address</td>\n'
+                '\t\t\t\t\t\t<td>534 Erewhon, Pleasantville, Vic, 3999</td>\n'
+                '\t\t\t\t\t</tr>\n'
+                '\t\t\t\t\t<tr>\n'
+                '\t\t\t\t\t\t<td>Contacts</td>\n'
+                '\t\t\t\t\t\t<td>Home: unknown. Work: (03) 5555 6473</td>\n'
+                '\t\t\t\t\t</tr>\n'
+                '\t\t\t\t\t<tr>\n'
+                '\t\t\t\t\t\t<td>Id</td>\n'
+                '\t\t\t\t\t\t<td>MRN: 12345 (Acme Healthcare)</td>\n'
+                '\t\t\t\t\t</tr>\n'
+                '\t\t\t\t</tbody>\n'
+                '\t\t\t</table>\n'
+                '\t\t</div>'
+          ]);
+    });
   });
 }
 
