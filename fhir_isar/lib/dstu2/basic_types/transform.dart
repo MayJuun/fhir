5aaa689822a2337293ae78f5d1ac965687e45641
Dokotela <grey.faulkenberry@mayjuun.com>
2023-05-02 22:36:15 -0400

    weird

--- fhir_isar/lib/dstu2/basic_types/transform.dart
+++ fhir_isar/lib/dstu2/basic_types/transform.dart
@@ -17,14 +17,48 @@ Future<void> main() async {
       var classFile = false;
       var isClass = false;
       final classVars = [];
+      var className = '';
       for (final line in fileList) {
         if (!classFile) {
           newText += '$line\n';
           if (line.contains('class')) {
+            className = line.replaceAll('class', '').replaceAll('{', '').trim();
             classFile = true;
             isClass = true;
           }
-        } else if (isClass) {}
+        } else if (isClass) {
+          if (line.contains('}')) {
+            isClass = false;
+            newText += 'const $className({\n';
+            classVars.forEach((element) {
+              final varList = element.toString().split(' ');
+              varList.removeWhere((e) => e == '' || e == ' ');
+              if (varList.length == 2) {
+                newText += 'this.${varList.last}\n';
+              } else if (varList.length > 2) {
+                newText += varList.sublist(0, varList.length - 2).join(' ');
+                newText += ' this.${varList.last}';
+              } else {
+                print('VarList: $varList');
+              }
+            });
+            newText += '});\n';
+            classVars.forEach((element) {
+              final varList = element.toString().split(' ');
+              varList.removeWhere((e) => e == '' || e == ' ');
+              if (varList.isNotEmpty) {
+                newText +=
+                    '${varList[varList.length - 2]} ${varList.last.replaceAll(",", ";")}';
+              }
+            });
+            newText += '}\n';
+            classVars.clear();
+          } else {
+            classVars.add(line);
+          }
+        } else {
+          print('Line: $line');
+        }
       }
       await File(file).writeAsString(newText);
     }
